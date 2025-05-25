270fb6ca45e3947ee50fee0b357e456b88603307
Pako182s <127125712+Pako182s@users.noreply.github.com>
2025-05-25 14:57:41 +0200

    Delete src/main/java/model/Documento.java

--- src/main/java/model/Documento.java
+++
@@ -1,9 +0,0 @@
-public class Documento {
-    private String documento;
-    private String commentoGiudice;
-
-    public Documento(String documento, String commentoGiudice) {
-        this.documento = documento;
-        this.commentoGiudice = commentoGiudice;
-    }
-}
